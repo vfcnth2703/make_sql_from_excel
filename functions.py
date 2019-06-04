9bc93d7e1f3fec4b10fdb57846ff012c808716d8
Alex <av.lobachev@gmail.com>
2019-06-04 08:53:57 +0300

    Удаление нулевых пробелов и приведение к нижнему регистру имя файла для корректного сравнения.

--- functions.py
+++ functions.py
@@ -10,7 +10,8 @@ def write_sql(header, body, file_name):
     with open(file_name, 'w') as f:
         f.write(f'WITH R ([ID], {header})\n AS (\n')
         for id, item in enumerate(body, start=1):
-            line = '\', \''.join(map(convert_num, item))
+            line = '\', \''.join(map(convert_num, item)).replace(u'\u200b','')
+            # replace(u'\u200b','') нужно для удаления нулевых пробелов.
             f.write(f'{add_space(4)}select {id},\'{line}\'\n')
             if item != body[-1]:
                 f.write(f'{add_space(6)}union all\n')
@@ -77,11 +78,11 @@ def collect_files(*args):
         Делает список файлов с заданными расширениями в
         текущей директории.
     """
-    args = tuple(['.' + item for item in args])
+    args = tuple(['.' + item.lower() for item in args])
     home_dir = os.getcwd()
     file_list = []
     for file in sorted(os.listdir(home_dir)):
-        if file.endswith(args):
+        if file.lower().endswith(args):
             file_list.append(home_dir + '\\' + file)
     return file_list
 
